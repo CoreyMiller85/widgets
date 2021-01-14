4692c3f4671d7d11e27d9453fb846e0d5fa1b828
Corey Miller <CoreyMiller454@gmail.com>
2021-01-13 20:08:26 -0500

    adds throttling to api requests

--- src/component/Search.js
+++ src/component/Search.js
@@ -6,27 +6,40 @@ const Search = () => {
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    const search = async () => {
-      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-        params: {
-          action: "query",
-          list: "search",
-          origin: "*",
-          format: "json",
-          srsearch: term,
-        },
-      });
-      setResults(data.query.search);
-    };
-    search();
+    console.log("inital render or term was changed");
+    // const search = async () => {
+    //   const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+    //     params: {
+    //       action: "query",
+    //       list: "search",
+    //       origin: "*",
+    //       format: "json",
+    //       srsearch: term,
+    //     },
+    //   });
+    //   setResults(data.query.search);
+    // };
+    // const timeoutId = setTimeout(() => {
+    //   if (term) {
+    //     search();
+    //   }
+    // }, 500);
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
