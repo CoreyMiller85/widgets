cc2fd69f915e105d1de10fbae55c675263d95893
Corey Miller <CoreyMiller454@gmail.com>
2021-01-30 21:48:33 -0500

    add timeout to search function

--- src/component/Search.js
+++ src/component/Search.js
@@ -7,23 +7,32 @@ const Search = () => {
 
   useEffect(() => {
     console.log("inital render or term was changed");
-    // const search = async () => {
-    //   const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-    //     params: {
-    //       action: "query",
-    //       list: "search",
-    //       origin: "*",
-    //       format: "json",
-    //       srsearch: term,
-    //     },
-    //   });
-    //   setResults(data.query.search);
-    // };
-    // const timeoutId = setTimeout(() => {
-    //   if (term) {
-    //     search();
-    //   }
-    // }, 500);
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
